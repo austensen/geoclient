f4ab5af3fdb6962d4023fc95c3c8f25b81ce9c66
Maxwell Austensen <maxwell.austensen@nyu.edu>
2018-12-12 00:34:31 -0500

    Refactor api install (#6)
    * improve valid bbl and bin checks, handle factor inputs, fix handling of invalid inputs, add/improve related tests
    
    * remove unused helper, add test for new helper
    
    * add codecov for pull requests
    
    * add Rtools for appveyor
    
    * remove reprecated arguments for glue-utils
    
    * refactor geoclient_api_keys functions for greater clarity

--- R/utils-glue.R
+++ R/utils-glue.R
@@ -1,13 +1,13 @@
 # Adapted from Jenny Bryan's googlesheets package
 
-msg_glue <- function(..., .envir = parent.frame(), type = NULL, call = FALSE) {
-  rlang::inform(glue::glue(..., .envir = .envir), type = type, call = call)
+msg_glue <- function(..., .envir = parent.frame(), .subclass = NULL) {
+  rlang::inform(glue::glue(..., .envir = .envir), .subclass = .subclass)
 }
 
-warn_glue <- function(..., .envir = parent.frame(), type = NULL, call = FALSE) {
-  rlang::warn(glue::glue(..., .envir = .envir), type = type, call = call)
+warn_glue <- function(..., .envir = parent.frame(), .subclass = NULL) {
+  rlang::warn(glue::glue(..., .envir = .envir), .subclass = .subclass)
 }
 
-stop_glue <- function(..., .envir = parent.frame(), type = NULL, call = FALSE) {
-  rlang::abort(glue::glue(..., .envir = .envir), type = type, call = call)
+stop_glue <- function(..., .envir = parent.frame(), .subclass = NULL) {
+  rlang::abort(glue::glue(..., .envir = .envir), .subclass = .subclass)
 }
