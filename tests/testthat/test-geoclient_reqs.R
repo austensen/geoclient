6047fe30f1f01689e171b1ba4887659b25a599a3
Maxwell Austensen <maxwell.austensen@nyu.edu>
2021-06-18 09:26:06 -0400

    fix typo in no_results test

--- tests/testthat/test-geoclient_reqs.R
+++ tests/testthat/test-geoclient_reqs.R
@@ -20,9 +20,9 @@ inputs_dup <- tibble(
 )
 
 test_that("placeholder dataframe still returned if inputs are invalid", {
-  multi_inputs <- tibble(houseNumber = rep(517, 2), street = rep("clinton st", 2), borough = c(NA, "mn"), zip = rep(NA, 2))
+  multi_inputs <- tibble(houseNumber = rep(500, 2), street = rep("clinton st", 2), borough = c(NA, "mn"), zip = rep(NA, 2))
   multi_ret <- geoclient_reqs(multi_inputs, creds = creds, operation = "address", rate_limit = TRUE)
-  expect_identical(select(multi_ret, no_results), tibble(no_results = c(FALSE, TRUE)))
+  expect_identical(select(multi_ret, no_results), tibble(no_results = c(TRUE, FALSE)))
 
   single_inputs <- slice(multi_inputs, 1)
   single_ret <- geoclient_reqs(single_inputs, creds = creds,  operation = "address", rate_limit = TRUE )
